9a6f103eb5bc8a22a2f135da923cc0aec24dad56
sakibb7 <venturesakib@gmail.com>
2022-07-03 15:58:54 +0600

    First Commit

---
+++ orders.js
@@ -0,0 +1,32 @@
+const Orders = [
+    {
+        productName: 'Foldable Mini Drone',
+        productNumber: '3437',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Larvender Drone',
+        productNumber: '43434',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Ruko Pro Drone',
+        productNumber: '33434',
+        paymentStatus: 'Due',
+        shipping: 'Pending'
+    },
+    {
+        productName: 'Drone With Camera',
+        productNumber: '3434',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+    {
+        productName: 'Drone with GPS',
+        productNumber: '4343',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered'
+    },
+]
